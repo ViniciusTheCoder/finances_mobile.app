106bbbec6852a7f7f73a47c448bb219d016c02f8
Vinícius Gurski Ferraz <vinigurskiferraz@gmail.com>
2022-06-01 22:12:39 -0300

    First commit

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,21 @@
+export default {
+    colors: {
+
+        primary: '#191C4D',
+
+        secondary: '#FFF700',
+        secondary_light: 'rgba(255, 247, 0, 0.3)',
+
+        sucess: '#12A454',
+        sucess_light: 'rgba(18, 164, 84, 0.5)',
+
+        attention: '#E83F5B',
+        attention_light: 'rgba(232, 63, 91, 0.5)',
+
+        shape: '#FFFFFF',
+        title: '#363F5F',
+        text: '#969CB2',
+        background: '#F0F2F5',
+
+    }
+}
