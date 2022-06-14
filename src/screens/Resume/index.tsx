c0a8c65e738b91a1c5b616e1e742c4f1a0908922
Vinícius Gurski Ferraz <vinigurskiferraz@gmail.com>
2022-06-14 20:00:15 -0300

    creating e stylin' the graph

--- src/screens/Resume/index.tsx
+++ src/screens/Resume/index.tsx
@@ -1,28 +1,133 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { VictoryPie } from "victory-native";
+import { RFValue } from "react-native-responsive-fontsize";
 
 import { HistoryCard } from "../../components/HistoryCard";
 
 import {
     Container,
     Header,
-    Title
+    Title,
+    Content,
+    ChartContainer
 } from "./styles";
 
+import { categories } from "../../utils/categories";
+
+
+interface TransactionData {
+    type: 'positive' | 'negative';
+    name: string;
+    amount: string;
+    category: string;
+    date: string;
+}
+
+interface CategoryData {
+    key: string;
+    name: string;
+    total: number;
+    totalFormatted: string;
+    color: string;
+    percent: string
+}
+
 export function Resume() {
+
+    const [totalByCategories, setTotalByCategories] = useState<CategoryData[]>([]);
+
+    async function loadData() {
+        const dataKey = '@gofinances: transactions';
+        const response = await AsyncStorage.getItem(dataKey);
+        const responseFormatted = response ? JSON.parse(response) : [];
+
+        const expansives = responseFormatted
+            .filter((expansive: TransactionData) => expansive.type === 'negative');
+
+        const expansivesTotal = expansives
+            .reduce((acumullator: number, expansive: TransactionData) => {
+                return acumullator + Number(expansive.amount);
+            }, 0);
+
+        const totalByCategory: CategoryData[] = [];
+
+        categories.forEach(category => {
+            let categorySum = 0;
+
+            expansives.forEach((expansive: TransactionData) => {
+                if (expansive.category === category.key) {
+                    categorySum += Number(expansive.amount)
+                }
+            });
+
+            if (categorySum > 0) {
+                const totalFormatted = categorySum.toLocaleString('pt-BR', {
+                    style: 'currency',
+                    currency: 'BRL'
+                })
+
+                const percent = `${(categorySum / expansivesTotal * 100).toFixed(0)}%`;
+
+                totalByCategory.push({
+                    key: category.key,
+                    name: category.name,
+                    color: category.color,
+                    total: categorySum,
+                    totalFormatted,
+                    percent
+                });
+            }
+        });
+
+        setTotalByCategories(totalByCategory);
+
+    }
+
+    useEffect(() => {
+        loadData();
+    }, []);
+
+
     return (
         <Container>
             <Header>
                 <Title>Resumo por categoria</Title>
             </Header>
 
+            <Content>
 
+                <ChartContainer>
 
-            <HistoryCard
-                title="Compras"
-                amount="R$150,50"
-                color="blue"
-            />
+                    <VictoryPie
+                        data={totalByCategories}
+                        colorScale={totalByCategories.map(category => category.color)}
+                        style={{
+                            labels: {
+                                fontSize: RFValue(18),
+                                fontWeight: 'bold',
+                                fill: '#FFF'
+                            }
+                        }}
+                        labelRadius={55}
+                        x={'percent'}
+                        y={'total'}
+                    />
 
+                </ChartContainer>
+
+                {
+                    totalByCategories.map(item => (
+                        <HistoryCard
+                            key={item.key}
+                            title={item.name}
+                            amount={item.totalFormatted}
+                            color={item.color}
+                        />
+                    ))
+                }
+
+            </Content>
         </Container>
     )
 }
