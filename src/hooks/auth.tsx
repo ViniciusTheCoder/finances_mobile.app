ac4ba9ebc5a0e9c52e8a2365ad471d938b611cfd
Vinícius Gurski Ferraz <vinigurskiferraz@gmail.com>
2022-06-20 16:01:48 -0300

    fixing som' problems

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -35,11 +35,7 @@ function AuthProvider({ children }: AuthProviderprops) {
             const RESPONSE_TYPE = 'token';
             const SCOPE = encodeURI('profile email');
 
-            const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?
-        client_id=${CLIENT_ID}&
-        redirect_uri=${REDIRECT_URI}&
-        response_type=${RESPONSE_TYPE}&
-        scope${SCOPE}`;
+            const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope${SCOPE}`;
 
             const response = await AuthSession.startAsync({ authUrl });
             console.log(response)
